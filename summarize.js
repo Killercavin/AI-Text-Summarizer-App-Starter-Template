53b8cb810c2f507af3b79b190ddaedd28e51f334
Killercavin <10078727-Killercavin@users.noreply.replit.com>
2024-01-26 23:08:13 +0000

    Initial commit

---
+++ summarize.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+async function summarizeText(text) {
+
+  // INSERT CODE SNIPPET FROM POSTMAN BELOW
+  // This is the code snippet from the postman request
+  let data = JSON.stringify({
+    "inputs": text,
+    "parameters": {
+      "max_length": 100,
+      "min_length": 30
+    }
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env['ACCESS_TOKEN']s
+    },
+    data: data
+  };
+
+  try {
+    const response = await axios.request(config);
+    return response.data[0].summary_text;
+  }
+  catch (error) {
+    console.log(error);
+  }
+
+}
+
+// Allows for summarizeText() to be called outside of this file
+
+module.exports = summarizeText;
